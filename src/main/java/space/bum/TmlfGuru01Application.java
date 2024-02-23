fbd1bd2555c915b10cdbef8c053dcf89f655731a
osparking <jbpark03@gmail.com>
2024-02-23 09:49:54 +0900

    프로젝트 생성 직후

---
+++ src/main/java/space/bum/TmlfGuru01Application.java
@@ -0,0 +1,13 @@
+package space.bum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmlfGuru01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmlfGuru01Application.class, args);
+	}
+
+}
---
+++ src/test/java/space/bum/TmlfGuru01ApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmlfGuru01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
