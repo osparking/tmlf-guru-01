9c944e8dd5feb0994e8ddf615bbcfc5c0e5e4621
osparking <jbpark03@gmail.com>
2024-03-16 20:59:49 +0900

    로켈 변경 처리기 빈 메소드

--- src/main/java/space/bum/thyleaf/config/SpringMvcConfig.java
+++ src/main/java/space/bum/thyleaf/config/SpringMvcConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
@@ -19,4 +20,10 @@ public class SpringMvcConfig implements WebMvcConfigurer {
     return localeResolver;
   }
 
+  @Bean
+  LocaleChangeInterceptor localeChangeInterceptor() {
+    LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+    localeChangeInterceptor.setParamName("lang");
+    return localeChangeInterceptor;
+  }
 }
