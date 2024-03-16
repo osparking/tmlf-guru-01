81903a685a9cbb337dbbf6addf42cf90eebfeb23
osparking <jbpark03@gmail.com>
2024-03-16 20:54:06 +0900

    로켈 결정자 빈 메소드

--- src/main/java/space/bum/thyleaf/config/SpringMvcConfig.java
+++ src/main/java/space/bum/thyleaf/config/SpringMvcConfig.java
@@ -1,9 +1,22 @@
 package space.bum.thyleaf.config;
 
+import java.util.Locale;
+import java.util.TimeZone;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
+  @Bean
+  LocaleResolver localeResolver() {
+    var localeResolver = new SessionLocaleResolver();
+    localeResolver.setDefaultLocale(Locale.KOREAN);
+    localeResolver.setDefaultTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+    return localeResolver;
+  }
 
 }
