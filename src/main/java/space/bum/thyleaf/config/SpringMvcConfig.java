b6743b08ad67dfca66b77d4236c4a658224fb645
osparking <jbpark03@gmail.com>
2024-03-16 19:04:52 +0900

    i18n 지원 클래스/빈 정의 기본

---
+++ src/main/java/space/bum/thyleaf/config/SpringMvcConfig.java
@@ -0,0 +1,9 @@
+package space.bum.thyleaf.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+}
