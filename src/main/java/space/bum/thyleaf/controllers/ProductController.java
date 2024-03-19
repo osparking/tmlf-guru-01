b9675a33dc368f24e295d7cff22ceaa77b801142
osparking <jbpark03@gmail.com>
2024-03-19 15:52:05 +0900

    상품 제어기에 서비스 참조 추가

--- src/main/java/space/bum/thyleaf/controllers/ProductController.java
+++ src/main/java/space/bum/thyleaf/controllers/ProductController.java
@@ -1,10 +1,21 @@
 package space.bum.thyleaf.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import space.bum.thyleaf.services.ProductService;
+
 @Controller
 public class ProductController {
+
+  private ProductService productService;
+
+  @Autowired
+  public void setProductService(ProductService productService) {
+    this.productService = productService;
+  }
+
   @GetMapping("/product")
   public String getProduct() {
     return "product";
