09b0947d485afb79039f9973fe8ca9f58696a55f
osparking <jbpark03@gmail.com>
2024-03-19 16:15:24 +0900

    ID 로 상품 요청할 때 처리 메소드

--- src/main/java/space/bum/thyleaf/controllers/ProductController.java
+++ src/main/java/space/bum/thyleaf/controllers/ProductController.java
@@ -2,7 +2,9 @@ package space.bum.thyleaf.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import space.bum.thyleaf.services.ProductService;
 
@@ -19,5 +21,11 @@ public class ProductController {
   @GetMapping("/product")
   public String getProduct() {
     return "redirect:/index";
+  }
+
+  @GetMapping("/product/{id}")
+  public String getProductById(@PathVariable("id") Integer id, Model model) {
+    model.addAttribute("product", productService.getProduct(id));
+    return "product";
   }
 }
