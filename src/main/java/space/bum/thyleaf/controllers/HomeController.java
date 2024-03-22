348ff203724b67bb21ab999f1593187ed9e681de
osparking <jbpark03@gmail.com>
2024-03-22 19:10:42 +0900

    제어기는 상품목록을 뷰로 넘김

--- src/main/java/space/bum/thyleaf/controllers/HomeController.java
+++ src/main/java/space/bum/thyleaf/controllers/HomeController.java
@@ -1,12 +1,25 @@
 package space.bum.thyleaf.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import space.bum.thyleaf.services.ProductService;
 
 @Controller
 public class HomeController {
+  
+  private ProductService productService;
+  
+  @Autowired
+  public void setProductService(ProductService productService) {
+    this.productService = productService;
+  }
+  
   @GetMapping
-  public String home() {
+  public String home(Model model) {
+    model.addAttribute("products", productService.listProducts());
     return "index";
   }
 }
