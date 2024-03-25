92f08ab78e52348536ecc0b25bf7b8132690cc6c
osparking <jbpark03@gmail.com>
2024-03-25 14:07:06 +0900

    상품 구매-계산 제어/처리기

---
+++ src/main/java/space/bum/thyleaf/controllers/CheckoutController.java
@@ -0,0 +1,30 @@
+package space.bum.thyleaf.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.validation.Valid;
+import space.bum.thyleaf.comands.CheckoutCommand;
+
+@Controller
+public class CheckoutController {
+
+  @GetMapping("/checkout")
+  public String checkoutProduct() {
+    return "checkoutform";
+  }
+
+  @PostMapping("/docheckout")
+  public String checkoutProduct(@Valid CheckoutCommand command,
+      BindingResult result) {
+    
+    if (result.hasErrors()) {
+      return "checkoutform";
+    } else {
+      return "checkoutdone";
+    }
+  }
+
+}
